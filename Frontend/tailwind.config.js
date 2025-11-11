0a003a62b875701bde08a28e7dff93baed662748
Lokesh Prajapati <136824294+prajapati-lokesh16@users.noreply.github.com>
2025-11-11 21:49:22 +0530

    Create tailwind.config.js

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      // Your custom theme extensions go here
+    },
+  },
+  plugins: [],
+};
