513a8cb9db9a62e50da8621e30c14e71571d2f51
Lokesh Prajapati <136824294+prajapati-lokesh16@users.noreply.github.com>
2025-11-11 21:48:04 +0530

    Update postcss.config.mjs

--- Frontend/postcss.config.mjs
+++ Frontend/postcss.config.mjs
@@ -1,5 +1,9 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.js
+module.exports = {
+  plugins: {
+    // 1. Tailwind CSS must be run first
+    'tailwindcss': {},
+    // 2. Autoprefixer should run after Tailwind
+    'autoprefixer': {},
+  },
 };
-
-export default config;
